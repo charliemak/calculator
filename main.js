7c88bf07d89f72bbdaba5ed853d56ab05d65158e
ctmakowski <ctmakowski@gmail.com>
2025-05-23 19:28:57 -0400

    feat(init): make initial commit for repo of pre-existing codebase

---
+++ main.js
@@ -0,0 +1,103 @@
+let buttons = document.querySelector('.calc-buttons');
+
+let state = {
+    operandString: "",
+    runningTotal : 0,
+    prevOperator : "",
+    screenBuffer : document.querySelector('.calc-screen')
+}
+
+function resetState(state)
+{
+    state.operandString = "";
+    state.runningTotal  = 0;
+    state.prevOperator  = "";
+    state.screenBuffer.textContent = "0";
+}
+
+function handleBackspace(state, buttonValue)
+{
+    if (buttonValue==="←")
+    {
+        state.operandString =   
+            state
+                .operandString
+                .substring(0, state.operandString.length - 1);
+
+        if (state.operandString)
+        {
+            state.screenBuffer.textContent = state.operandString;
+        }
+        // TODO: need to ignore backspace if screenBuffer isn't empty
+        else 
+        {
+            state.screenBuffer.textContent = "0";
+        }
+    }
+}
+
+function init() {
+
+    buttons.addEventListener('click', function(event) {
+        let value = event.target.textContent.trim();
+        let intValue = parseInt(value);
+
+        if (!isNaN(intValue))
+        {
+            if (!state.operandString)
+            {
+                if (intValue)
+                {
+                    state.operandString = value;
+                    state.screenBuffer.textContent = value;
+                }
+            }
+            else 
+            {
+                state.operandString += value;
+                state.screenBuffer.textContent += value;
+            }
+
+        }
+
+        else if (value==="C")
+        {
+            resetState(state);
+        }
+
+        else if (value==="←")
+        {
+            handleBackspace(state, value);
+        }
+
+        else if ("+−×÷=".includes(value))
+        {
+            switch (state.prevOperator) {
+                case "+":
+                    state.runningTotal += parseInt(state.operandString);
+                    break;
+                case "−":
+                    state.runningTotal -= parseInt(state.operandString);
+                    break;
+                case "×":
+                    state.runningTotal *= parseInt(state.operandString);
+                    break;
+                case "÷":
+                    state.runningTotal /= parseInt(state.operandString);
+                    break;
+                case "=":
+                    break;        
+                default:
+                    state.runningTotal = parseInt(state.operandString);
+                    break;
+            }
+
+            state.screenBuffer.textContent = state.runningTotal;
+            state.prevOperator = value;
+            state.operandString = "";
+        }
+    })
+}
+
+
+init()
